a38d30f84bf1875658bb110132362db95dd1c2bf
Anna <53200657+CaptainPaws@users.noreply.github.com>
2026-01-21 11:04:27 +0400

    Add advancedfontawesome addon

---
+++ inc/plugins/advancedfunctionality/addons/advancedfontawesome/manifest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * AF Addon Manifest: AdvancedFontAwesome
+ */
+return [
+    'id'       => 'advancedfontawesome',
+    'name'     => 'Advanced Font Awesome',
+    'version'  => '1.0.0',
+    'author'   => 'CaptainPaws',
+    'bootstrap'=> 'advancedfontawesome.php',
+
+    'lang' => [
+        'front' => [
+            'af_advancedfontawesome_name'        => 'Advanced Font Awesome',
+            'af_advancedfontawesome_description' => 'Глобальное подключение Font Awesome, иконки форумов и кнопка [fa] в редакторе.',
+        ],
+        'admin' => [
+            'af_advancedfontawesome_group'       => 'Advanced Font Awesome',
+            'af_advancedfontawesome_group_desc'  => 'Настройки Font Awesome и иконок форумов.',
+            'af_advancedfontawesome_icon_label'  => 'Иконка',
+            'af_advancedfontawesome_icon_desc'   => 'Укажите класс Font Awesome (например: fa-solid fa-star).',
+            'af_advancedfontawesome_icon_search' => 'Поиск иконок...',
+            'af_advancedfontawesome_admin_title' => 'Font Awesome для форумов',
+        ],
+    ],
+
+    'admin' => [
+        'slug'       => 'advancedfontawesome',
+        'controller' => 'admin.php',
+    ],
+];
