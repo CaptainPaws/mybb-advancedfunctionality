641b777164d9c304ec9a8229279795441fa0419e
Anna <53200657+CaptainPaws@users.noreply.github.com>
2026-02-26 20:49:46 +0400

    Update balancemanage.php

--- inc/plugins/advancedfunctionality/addons/balance/assets/balancemanage.php
+++ inc/plugins/advancedfunctionality/addons/balance/assets/balancemanage.php
@@ -4,7 +4,7 @@
  */
 define('IN_MYBB', 1);
 define('THIS_SCRIPT', 'balancemanage.php');
-require_once __DIR__ . '/../../../../../../global.php';
+require_once __DIR__ . '/global.php';
 
 if (function_exists('af_balance_render_balancemanage')) {
     af_balance_render_balancemanage();
