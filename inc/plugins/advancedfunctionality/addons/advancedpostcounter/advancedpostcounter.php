f92c02e7a3c0bb7c252ef84a7aedc0f77009c09f
Anna <53200657+CaptainPaws@users.noreply.github.com>
2026-09-26 07:17:33 +0400

    Fix AdvancedMenu item location routing

--- inc/plugins/advancedfunctionality/addons/advancedpostcounter/advancedpostcounter.php
+++ inc/plugins/advancedfunctionality/addons/advancedpostcounter/advancedpostcounter.php
@@ -21,7 +21,7 @@ const AF_APC_DEFAULT_ASSETS_BLACKLIST = "index.php\nforumdisplay.php\nusercp.php
 
 function af_advancedpostcounter_menu_provider(): void
 {
-    af_menu_register_item(['key'=>'post_activity','source_addon'=>AF_APC_ID,'label'=>'Постовая активность','icon'=>'fa-solid fa-chart-column','type'=>'link','default_container'=>'secondary','default_sortorder'=>60,'visibility'=>true,'action'=>['url'=>'postsactivity.php']]);
+    af_menu_register_item(['key'=>'post_activity','source_addon'=>AF_APC_ID,'label'=>'Постовая активность','icon'=>'fa-solid fa-chart-column','type'=>'link','section'=>'links','default_container'=>'user_drawer','allowed_containers'=>['user_drawer'],'default_sortorder'=>60,'visibility'=>true,'action'=>['url'=>'postsactivity.php']]);
 }
 
 
